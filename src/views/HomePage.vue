3b19ea26870d43863bb9398c0cf10691ddbc6a79
Oguzhan <oguzhanmemis266@gmail.com>
2022-01-04 14:55:29 +0300

    axios added

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -27,7 +27,13 @@
 </template>
 
 <script>
-export default {};
+export default {
+  created(){
+    this.$appAxios.get("/bookmarks").then(bookmarks_list_response => {
+      console.log(bookmarks_list_response);
+    })
+  }
+};
 </script>
 
 <style></style>
--- src/views/NewBookmark.vue
+++ src/views/NewBookmark.vue
@@ -4,6 +4,7 @@
       <label for="title" class="form-label" id="title">Başlık</label>
       <input
         type="text"
+        v-model="userData.title"
         class="form-control"
         id="exampleFormControlInput1"
         placeholder="Aramak istediğinizi yazınız."
@@ -13,6 +14,7 @@
       <label for="url" class="form-label" id="url">URL</label>
       <input
         type="text"
+        v-model="userData.url"
         class="form-control"
         id="exampleFormControlInput1"
         placeholder="https://..."
@@ -23,21 +25,46 @@
         >Açıklama</label
       >
       <textarea
+        v-model="userData.description"
         class="form-control"
         placeholder="Bu var ya..."
         id="dexcription"
         rows="3"
       ></textarea>
     </div>
+    <!-- @click="$router.go(-1)" bir önceki sayfaya gider -->
     <div class="d-flex justify-content-end align-items-center">
       <button class="btn btn-sm btn-secondary me-1" @click="$router.push({ name : 'HomePage' })">İptal</button>
-      <button class="btn btn-sm btn-primary">Kaydet</button>
+      <button class="btn btn-sm btn-primary" @click="onSave">Kaydet</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return {
+      userData : {
+        title : null,
+        url : null,
+        description : null
+      }
+    }
+  },
+  methods: {
+    onSave(){
+      console.log(this.userData);
+      this.$appAxios.post("/bookmarks", this.userData).then(save_response => {
+        console.log("save_response", save_response);
+        this.resetData();
+      });
+    },
+    //New alanının inputlarını ve textarea'yı temizler
+    resetData(){
+      Object.keys(this.userData).forEach(key => (this.userData[key] = null));
+    }
+  }
+};
 </script>
 
 <style></style>
