78e0ef88bd540d56aa8e64107d81d764262fb10f
Oguzhan <oguzhanmemis266@gmail.com>
2022-01-04 14:13:06 +0300

    first commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card border b-2">
+    <div class="d-flex justify-content-end">
+      <button class="btn btn-sm btn-primary m-2" @click="$router.push({ name:'NewBookmark' })">+ Yeni Ekle</button>
+    </div>
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Başlık</th>
+          <th scope="col">URL</th>
+          <th scope="col">Sil</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="i in 10" :key="i">
+          <th scope="row">1</th>
+          <td>Vue3 Dokumantasyon</td>
+          <td>https://v3.vuejs.org</td>
+          <td>
+            <button class="btn btn-sm btn-danger">Sil</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/NewBookmark.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="form-area card border p-2">
+    <div class="mb-3">
+      <label for="title" class="form-label" id="title">Başlık</label>
+      <input
+        type="text"
+        class="form-control"
+        id="exampleFormControlInput1"
+        placeholder="Aramak istediğinizi yazınız."
+      />
+    </div>
+    <div class="mb-3">
+      <label for="url" class="form-label" id="url">URL</label>
+      <input
+        type="text"
+        class="form-control"
+        id="exampleFormControlInput1"
+        placeholder="https://..."
+      />
+    </div>
+    <div class="mb-3">
+      <label for="description" class="form-label" id="description"
+        >Açıklama</label
+      >
+      <textarea
+        class="form-control"
+        placeholder="Bu var ya..."
+        id="dexcription"
+        rows="3"
+      ></textarea>
+    </div>
+    <div class="d-flex justify-content-end align-items-center">
+      <button class="btn btn-sm btn-secondary me-1" @click="$router.push({ name : 'HomePage' })">İptal</button>
+      <button class="btn btn-sm btn-primary">Kaydet</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
